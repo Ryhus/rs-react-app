19e59bf2cd3acf084989c020f91e4dcdc9612d96
Ryhus <ryhusyevhen@gmail.com>
2025-07-24 16:11:20 +0200

    refactor(component): refactor the BreedList to the functional one

--- src/components/BreedList/BreedList.tsx
+++ src/components/BreedList/BreedList.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import BreedCard from '../BreedCard/BreedCard';
 import type { Breed } from '../../Services/DogService/types';
 
@@ -8,16 +7,14 @@ interface BreedListProps {
   breeds: Breed[];
 }
 
-class BreedList extends Component<BreedListProps> {
-  render(): React.ReactNode {
-    return (
-      <section className="breed-list">
-        {this.props.breeds.map((breed) => (
-          <BreedCard key={breed.id} breed={breed} />
-        ))}
-      </section>
-    );
-  }
+function BreedList({ breeds }: BreedListProps) {
+  return (
+    <section className="breed-list">
+      {breeds.map((breed) => (
+        <BreedCard key={breed.id} breed={breed} />
+      ))}
+    </section>
+  );
 }
 
 export default BreedList;
