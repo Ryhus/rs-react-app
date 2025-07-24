7edf0b3174476b4fef7984187754fd39ee18b53d
Ryhus <ryhusyevhen@gmail.com>
2025-07-24 16:01:37 +0200

    refactor(component): refactor the BreedCard component to the functional one

--- src/components/BreedCard/BreedCard.tsx
+++ src/components/BreedCard/BreedCard.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import type { Breed } from '../../Services/DogService/types';
 
 import './BreedCardStyles.scss';
@@ -7,17 +6,13 @@ interface BreedProps {
   breed: Breed;
 }
 
-class BreedCard extends Component<BreedProps> {
-  render(): React.ReactNode {
-    const { breed } = this.props;
-
-    return (
-      <article className="breed-card">
-        <img src={breed.image?.url} alt={breed.name}></img>
-        <h2>{breed.name}</h2>
-      </article>
-    );
-  }
+function BreedCard({ breed }: BreedProps) {
+  return (
+    <article className="breed-card">
+      <img src={breed.image?.url} alt={breed.name} />
+      <h2>{breed.name}</h2>
+    </article>
+  );
 }
 
 export default BreedCard;
