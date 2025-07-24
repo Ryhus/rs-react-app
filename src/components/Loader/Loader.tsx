1bfe5b2a92e1293b7960a4b7fb69608e6b21415e
Ryhus <ryhusyevhen@gmail.com>
2025-07-24 15:17:55 +0200

    refactor(component): refactor Loader component to functional one

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,15 +1,11 @@
-import { Component } from 'react';
-
 import './LoaderStyles.scss';
 
-class Loader extends Component {
-  render(): React.ReactNode {
-    return (
-      <div className="loader-wrapper" data-testid="loader-wrapper">
-        <div className="dog-walk" data-testid="dog-walk" />
-      </div>
-    );
-  }
+function Loader() {
+  return (
+    <div className="loader-wrapper" data-testid="loader-wrapper">
+      <div className="dog-walk" data-testid="dog-walk" />
+    </div>
+  );
 }
 
 export default Loader;
