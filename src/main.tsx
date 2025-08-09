b47cc725862f0d51e70a273229319aea58a75966
Ryhus <ryhusyevhen@gmail.com>
2025-08-09 10:55:06 +0200

    feat(TanStack query migration): setup QueryClient

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,22 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ErrorBoundary } from '@/components/';
 import { ThemeProvider } from '@/contexts';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.tsx';
 
 import './styles/main.scss';
 
 const rootElement = document.getElementById('root');
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 2,
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 if (!rootElement) {
   throw new Error('Root element not found');
@@ -16,7 +27,9 @@ createRoot(rootElement).render(
   <StrictMode>
     <ErrorBoundary>
       <ThemeProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </ThemeProvider>
     </ErrorBoundary>
   </StrictMode>
