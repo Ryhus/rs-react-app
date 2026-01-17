3292080a1d759c7197bcd462fd7bff17ed333ce1
Ryhus <ryhusyevhen@gmail.com>
2026-01-17 14:13:33 +0100

    refactor: change the link for the breed-image inside BreedDetails component, add .env.example

--- src/components/BreedDetails/BreedDetails.tsx
+++ src/components/BreedDetails/BreedDetails.tsx
@@ -27,11 +27,7 @@ function BreedDetails() {
   return (
     <div className="breed-details">
       {breed?.reference_image_id && (
-        <img
-          src={`https://cdn2.thedogapi.com/images/${breed.reference_image_id}.jpg`}
-          alt={breed.name}
-          className="breed-image"
-        />
+        <img src={breed.image?.url} alt={breed.name} className="breed-image" />
       )}
       <h2>{breed?.name}</h2>
 
